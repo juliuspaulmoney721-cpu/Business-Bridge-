3687bc5f62ad019d867669309974a1ab88a49d72
juliuspaulmoney721-cpu <julius.paulmoney721@gmail.com>
2026-08-19 07:30:14 +0100

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,7 +1,6 @@
-import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js@2/+esm";
+import { createClient } from "https://cdn.jsdelivr.net/npm/@supabase/supabase-js/+esm";
 
 const supabaseUrl = "https://qydwvspemujwizsrtxla.supabase.co";
-
-const supabaseKey = "sb_publishable_u2Ri0HpNit3DBB4owj7tb6w_t2IGUZW9";
+const supabaseKey = "sb_publishable_u2Ri0HpNit3DB4owj7tb6w_t2IGUZW9";
 
 export const supabase = createClient(supabaseUrl, supabaseKey);
