8d2bf9cd381c5f69087ff386684b45ff15809c2b
juliuspaulmoney721-cpu <julius.paulmoney721@gmail.com>
2026-08-20 12:13:05 +0100

    Add files via upload

--- supabase.js
+++ supabase.js
@@ -1,13 +1,7 @@
 import { createClient } from 'https://esm.sh/@supabase/supabase-js@2'
 
-const supabaseUrl = 'https://qydwvspemujwizsrtxla.supabase.co'
+const supabaseUrl = 'https://ycjlibjvhimhbpxpejqp.supabase.co'
 
 const supabaseKey = 'sb_publishable_3oaFbXriLxj3_PZ7nJfdYA_ppQmaMjM'
 
-export const supabase = createClient(supabaseUrl, supabaseKey, {
-  auth: {
-    persistSession: true,
-    autoRefreshToken: true,
-    detectSessionInUrl: true
-  }
-})
+export const supabase = createClient(supabaseUrl, supabaseKey)
